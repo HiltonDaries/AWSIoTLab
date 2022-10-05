bff6e76131f08c1a1a366e3190aea808dcc7034c
Hilton Daries <hilton.daries@gmail.com>
2022-10-05 20:41:42 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,12 +6,12 @@
 pip install AWSIoTPythonSDK
 
 # install jq
-yum install jq -y
+sudo yum install jq -y
 #apt-get install jq -y
 
 # get endpoint address
-aws iot describe-endpoint --region us-east-1 > endpoint.json
+# aws iot describe-endpoint --region us-east-1 > endpoint.json
 
 # set  endpoint environment variable
-ENDPOINT=$(jq -r '.endpointAddress' endpoint.json)
-echo $ENDPOINT
+#ENDPOINT=$(jq -r '.endpointAddress' endpoint.json)
+#echo $ENDPOINT
