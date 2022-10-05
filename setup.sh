d3725225dc52d8e4c1e15adb142879559da97ce2
HiltonDaries <hilton.daries@gmail.com>
2022-10-05 17:55:03 +0200

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# download IoT scripts 
+git clone https://github.com/HiltonDaries/AWSIoTLab.git
+
+# install AWS IoT Python SDK 
+pip install AWSIoTPythonSDK
+
+# install jq
+yum install jq -y
+
+# get endpoint address
+aws iot describe-endpoint --region us-east-1 > endpoint.json
+
+# set  endpoint environment variable
+ENDPOINT=$(jq -r '.endpointAddress' endpoint.json)
+echo $ENDPOINT
