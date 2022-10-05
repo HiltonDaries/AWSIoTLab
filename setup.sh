6557a915c785dd22a69fbd59569aca9561407ab7
Hilton Daries <hilton.daries@gmail.com>
2022-10-05 19:35:05 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,7 +6,8 @@
 pip install AWSIoTPythonSDK
 
 # install jq
-yum install jq -y
+#yum install jq -y
+apt isntall jq -y
 
 # get endpoint address
 aws iot describe-endpoint --region us-east-1 > endpoint.json
