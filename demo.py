50a8b1fc2b15a77dd963a6d9da846b1fa620c5f2
Hilton Daries <hilton.daries@gmail.com>
2022-10-06 09:03:40 +0200

    Update demo.py

--- demo.py
+++ demo.py
@@ -13,11 +13,12 @@ import uuid
 #Setup our MQTT client and security certificates
 #Make sure your certificate names match what you downloaded from AWS IoT
 
-mqttc = AWSIoTMQTTClient(uuid.uuid4())
+clientId = str(uuid.uuid4())
+mqttc = AWSIoTMQTTClient(clientId)
 
 #Use the endpoint from the settings page in the IoT console
-mqttc.configureEndpoint("a2cf8kt6n00n98.iot.us-west-2.amazonaws.com",8883)
-mqttc.configureCredentials("./rootCA.pem","./privateKey1.pem","./certificate1.pem")
+mqttc.configureEndpoint("a2cf8kt6n00n98-ats.iot.us-east-1.amazonaws.com",8883)
+mqttc.configureCredentials("rootCA.pem","privateKey1.pem","certificate1.pem")
 
 #Function to encode a payload into JSON
 def json_encode(string):
